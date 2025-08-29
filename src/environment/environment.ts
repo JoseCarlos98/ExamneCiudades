e0fb873effb731ec9a115bb0054189500300593a
JoseCarlos98 <54569527+JoseCarlos98@users.noreply.github.com>
2025-08-29 13:35:04 -0700

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    API_KEY_GOOGLE_MAPS : 'AIzaSyBqcgLkxHWsTedU6OVslI20Kh7mirh5UmU'
+    API_KEY_GOOGLE_MAPS : 'AIzaSyBqcgLkxHWsTedU6OVslI20Kh7mirh5UmU',
+    API_CIUDADES : 'https://restcountries.com/v3.1',
+    API_TIMEZONE : 'https://worldtimeapi.org/api/timezone',
 }
